c0715b40c8080b8139282833a1f6207936b3f729
vandanaannam <83237431+vandanaannam@users.noreply.github.com>
2026-05-31 18:36:02 +0530

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.java
@@ -0,0 +1,25 @@
+class TimeMap {
+    Map<Integer, List<String>> s = new HashMap<>();
+
+    public TimeMap() {
+        
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        s.put(timestamp, Arrays.asList(key,value));
+    }
+    
+    public String get(String key, int timestamp) {
+        while(timestamp>0){
+            
+            if(s.get(timestamp)!=null){
+                return s.get(timestamp).get(1);
+            }
+            else{
+                System.out.println("In");
+                timestamp--;
+            }
+        }
+        return "";
+    }
+}
