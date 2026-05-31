ee1259f6b82dc2accc7d07002ea17793609fe71e
vandanaannam <83237431+vandanaannam@users.noreply.github.com>
2026-05-31 16:48:16 +0530

    Add: search-2d-matrix - submission-9

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-9.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int cl = (matrix[0]).length;
+        int rl = matrix.length;
+        int f=0;
+        // if(cl==0 || rl ==0){
+        //     if(matrix[0][0]==target){
+        //         return true;
+        //     }
+        //     return false;
+        // }
+        for(int i=0 ; i<rl ; i++){
+            f++;
+            if(matrix[i][cl-1] > target){
+                System.out.println("--");
+                
+                break;
+            }
+        }
+        System.out.println(f);
+        for(int j =0 ; j < cl ; j++){
+            if(f!=0){
+               if(matrix[f-1][j] == target){
+                return true;
+                } 
+            }
+            // else{
+            //     if(matrix[f][j] == target){
+            //     return true;}
+            // }
+            
+        }
+        return false;
+    }
+}
