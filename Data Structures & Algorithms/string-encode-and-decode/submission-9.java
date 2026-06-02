beb141c0145e65cc42e3869925b8c4b7b1e54c51
vandanaannam <83237431+vandanaannam@users.noreply.github.com>
2026-06-02 11:33:16 +0530

    Add: string-encode-and-decode - submission-9

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-9.java
@@ -0,0 +1,44 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        String res = "";
+        for(String s: strs){
+            if(!res.isEmpty()){
+                res = res + "##" + s.length() + s;
+            }
+            else{
+                res = s.length() + s;
+            }
+        }
+        return res;
+    }
+
+    public List<String> decode(String str) {
+        if (str == null || str.isEmpty()) {
+            return new ArrayList<>();
+}
+        String[] res = str.split("##");
+        List<String> r = new ArrayList<>();
+        for(String s : res){
+            if(s.charAt(0)!=0){
+                int i = 0;
+
+                while (i < s.length() &&
+                s.charAt(i) >= '0' &&
+                s.charAt(i) <= '9') {
+                    i++;
+                }
+
+                r.add(s.substring(i));
+            }
+            else if(s.charAt(0)==0 && s.length()>1){
+                r.add(s.substring(1));
+            }
+            else{
+                r.add("");
+            }
+        }
+        
+        return r;
+    }
+}
