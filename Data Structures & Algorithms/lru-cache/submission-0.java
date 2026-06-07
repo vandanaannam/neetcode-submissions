57ad193e218fce9949952fb45cccf1090b584fe8
vandanaannam <83237431+vandanaannam@users.noreply.github.com>
2026-06-07 08:53:05 +0530

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.java
@@ -0,0 +1,26 @@
+class LRUCache {
+    Map<Integer,Integer> res = new HashMap<>();
+    public LRUCache(int capacity) {
+        
+    }
+    
+    public int get(int key) {
+        if(res.containsKey(key)){
+            return res.get(key);}
+        else{
+            return -1;
+        }
+    }
+    
+    public void put(int key, int value) {
+        if(res.size()<2){
+            res.put(key,value);
+        }
+        else{
+            for(int k: res.keySet()){
+                res.remove(k);
+                break;
+            }
+        }
+    }
+}
