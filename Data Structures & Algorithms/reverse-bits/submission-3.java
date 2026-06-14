441c96d465a913f84de626f47fc359ed0356bc3e
vandanaannam <83237431+vandanaannam@users.noreply.github.com>
2026-06-14 22:01:33 +0530

    Add: reverse-bits - submission-3

---
+++ Data Structures & Algorithms/reverse-bits/submission-3.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverseBits(int n) {
+        int res = 0;
+        for(int i=0; i<32 ; i++){
+            int r = n%2;
+            n = n>>>1;
+            res = res | (r << (31 -i));
+        }
+        return res;
+    }
+}
