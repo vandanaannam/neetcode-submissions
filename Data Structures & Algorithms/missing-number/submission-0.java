b4932f8fcc358760080489926ff2e2524a338347
vandanaannam <83237431+vandanaannam@users.noreply.github.com>
2026-06-14 22:05:46 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        for(int i=0; i< nums.length; i++){
+            if(nums[i]!=i){
+                return i;
+            }
+        }
+        return 0;
+    }
+}
