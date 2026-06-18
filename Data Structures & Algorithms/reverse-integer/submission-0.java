57af35f142ddb822484b82092c3c9489532200fa
vandanaannam <83237431+vandanaannam@users.noreply.github.com>
2026-06-18 09:13:37 +0530

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int x) {
+        Boolean isb = false;
+        if(x<0){
+            isb = true;
+        }
+        String str = new StringBuilder(String.valueOf(Math.abs(x))).reverse().toString();
+        long n = Long.parseLong(str);
+        if(isb){
+            n = -n;
+        }
+        if(n< Integer.MIN_VALUE || n > Integer.MAX_VALUE){
+            return 0;
+        }
+        return (int) n;
+    }
+}
